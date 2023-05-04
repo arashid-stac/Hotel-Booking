dac4197c08de0435d8a8549a5bf2d6cfbc7e969b
Alli Rashid <allirashid97@gmail.com>
2023-05-04 19:56:59 -0300

    created hotel and reservation ticket classes

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
-# This is a sample Python script.
+import pandas as pd
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+df = pd.read_csv("hotels.csv", dtype={"id": str})
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class Hotel:
+    def __init__(self, hotel_id):
+        self.hotel_id = hotel_id
+        self.name = df.loc[df["id"] == self.hotel_id, "name"].squeeze()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def book(self):
+        """Books a hotel by changing its availability to 'no'"""
+        df.loc[df["id"] == self.hotel_id, "available"] = "no"
+        df.to_csv("hotels.csv", index=False)
+
+    def available(self):
+        """Check if hotel is available"""
+        availability = df.loc[df["id"] == self.hotel_id, "available"].squeeze()
+        if availability == "yes":
+            return True
+        else:
+            return False
+
+
+class Reservation_Ticket:
+    def __init__(self, customer_name, hotel_object):
+        self.customer_name = customer_name
+        self.hotel = hotel_object
+
+    def generate(self):
+        content = f"""
+        Thank you for your reservation!
+        Here are your booking data:
+        Name: {self.customer_name}
+        Hotel name: {self.hotel.name}
+        """
+        return content
+
+
+print(df)
+hotel_ID = input("Enter the ID of the hotel: ")
+hotel = Hotel(hotel_ID)
+if hotel.available():
+    hotel.book()
+    name = input("Enter your name: ")
+    reservation_ticket = Reservation_Ticket(name, hotel)
+    print(reservation_ticket.generate())
+else:
+    print("Hotel is not available.")
